cc02f6e67796498ec914fa9a026614976aa8a061
Adam Medziński <medzin@users.noreply.github.com>
2018-03-23 16:29:06 +0100

    Add integration tests with Minikube (#8)
    This commit adds a simple integration test on Kubernetes cluster started
    with Minikube. It will create a pod with simple Python application and
    Consul agent in development mode. Then it checks if hook registers
    service with desired name.

---
+++ scripts/install_local_k8s.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CHANGE_MINIKUBE_NONE_USER=true
+
+# download and install kubectl, which is a requirement of minikube
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.8.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+
+# download and install minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+# start minikube with none driver (it setups localkube on the host)
+sudo minikube start --vm-driver=none --kubernetes-version=v1.8.0
+
+# update kubeconfig
+minikube update-context
+
+# wait for Kubernetes to be up and ready
+JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'
+until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
