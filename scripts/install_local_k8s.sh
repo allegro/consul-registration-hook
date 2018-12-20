69f1d8b377b5c470198da6922f47f9af11bbd00a
Tomasz Ziarko <tomasz.ziarko@allegro.pl>
2018-12-20 13:56:18 +0100

    Change consul tags source from k8s labels to annotations.
    According to official documentation labels doesn't allow to store non alphanumeric keys or values, in contrast to annotations (but only values).

--- scripts/install_local_k8s.sh
+++ scripts/install_local_k8s.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 CHANGE_MINIKUBE_NONE_USER=true
 K8S_VERSION=v1.9.4
+MINIKUBE_VERSION=v0.24.1
 
 # download and install kubectl, which is a requirement of minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
 # download and install minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 
 # disable crash reports
 minikube config set WantReportErrorPrompt false
-
 # start minikube with none driver (it setups localkube on the host)
 # see: https://github.com/kubernetes/minikube/issues/2704
 sudo minikube start --bootstrapper=localkube --logtostderr --v=0 --vm-driver=none --kubernetes-version=${K8S_VERSION}
